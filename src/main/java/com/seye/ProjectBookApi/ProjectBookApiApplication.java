939c32682fa87bb24e4aa326dbcae89c19f6b535
Victor Olaleye <vansvictor79@gmail.com>
2025-10-17 18:41:19 +0100

    Added sslmode=require to the application properties

--- src/main/java/com/seye/ProjectBookApi/ProjectBookApiApplication.java
+++ src/main/java/com/seye/ProjectBookApi/ProjectBookApiApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ProjectBookApiApplication {
+public class  ProjectBookApiApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ProjectBookApiApplication.class, args);
 	}
